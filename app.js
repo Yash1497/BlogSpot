2f8b8cc165520783cf68ca9b5b45eb2da6e3ae88
chiragprajapati781 <chiragprajapati781@gmail.com>
2018-02-10 06:20:24 -0500

    new commit

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
 var express = require('express');
 var app = express();
 var methodOverride = require('method-override');
-app.set("view engine", "ejs");
 var bodyParser = require("body-parser");
 var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy  = require('passport-local');
+var User = require('./models/user');
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 mongoose.connect("mongodb://localhost/restfull_blog");
-
 app.use(methodOverride("_method"));
+
+
+
+
+
+
+/////////////////////////////passport config///////////////////////////////////////////////
+
+app.use(require("express-session")({
+  secret:"THIS IS SECRET",
+  resave:false,
+  saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.use(function(req,res,next){
+  res.locals.currentUser = req.user;
+  next();
+}); 
+
+
+
 ///////////////////////////////////////////////Declaring Schema//////////////////////////////////////
 
 var blogSchema = new mongoose.Schema({
@@ -23,10 +51,10 @@ var Blog = mongoose.model("Blog", blogSchema)
 
 
 app.get("/", function(req, res) {
-  res.redirect("/blogs");
+  res.render("blog");
 });
 
-app.get("/blogs", function(req, res) {
+app.get("/blogs",isLoggedIn, function(req, res) {
   Blog.find({},function(err, blogs){
     if(err){
       console.log(err)
@@ -38,14 +66,14 @@ app.get("/blogs", function(req, res) {
 
 ///////////////////////////////////////new post////////////////////////////////////////////
 
-app.get("/blogs/new",function(req,res){
+app.get("/blogs/new",isLoggedIn,function(req,res){
   res.render("new");
 });
 
-///////////////////////////////////////post routes////////////////////////////////////////////
 
 
-app.post("/blogs",function(req,res){
+
+app.post("/blogs",isLoggedIn,function(req,res){
   Blog.create(req.body.blog,function(err,newBlog){
     if(err){
       console.log(err);
@@ -58,7 +86,7 @@ app.post("/blogs",function(req,res){
 ////////////////////////////////////////show routes//////////////////////////
 
 
-app.get("/blogs/:id",function(req, res){
+app.get("/blogs/:id",isLoggedIn,function(req, res){
   Blog.findById(req.params.id, function(err,foundBlogs){
       if(err){
     console.log(err)
@@ -70,7 +98,7 @@ app.get("/blogs/:id",function(req, res){
 
 /////////////////////////////////////edit route/////////////////////////////
 
-app.get("/blogs/:id/edit",function(req, res){
+app.get("/blogs/:id/edit",isLoggedIn,function(req, res){
    Blog.findById(req.params.id, function(err,foundBlogs){
       if(err){
     console.log(err)
@@ -81,9 +109,9 @@ app.get("/blogs/:id/edit",function(req, res){
 });
 
 
-/////////////////////////////////////////update blog//////////////////////////////
+/////////////////////////////////////////update button blog//////////////////////////////
 
-app.put("/blogs/:id", function(req, res){
+app.put("/blogs/:id",isLoggedIn, function(req, res){
   Blog.findByIdAndUpdate(req.params.id, req.body.blog ,function(err,updateBlogs){
      if(err){
     console.log(err)
@@ -96,7 +124,7 @@ app.put("/blogs/:id", function(req, res){
 
 ///////////////////delete routes////////////////////////////////////
 
-app.delete("/blogs/:id",function(req,res){
+app.delete("/blogs/:id",isLoggedIn,function(req,res){
   Blog.findByIdAndRemove(req.params.id, function(err){
      if(err){
     console.log(err)
@@ -106,12 +134,54 @@ app.delete("/blogs/:id",function(req,res){
    });
 });
 
+/////////////////////////user sign in////////////////////////////////////////
+
+app.get("/register",function(req,res){
+  res.render("register");
+});
+
+app.post("/register",function(req,res){
+  var newUser = new User({username:req.body.username});
+  var password = req.body.password; 
+  User.register(newUser, password, function(err, user){
+    if(err){
+      console.log(err);
+      
+    } else{
+       passport.authenticate("local")(req, res, function(){
+       res.redirect("/blogs");
+    });
+    }
+  });
+});
 
 
 
+///////////////////////////log in/////////////////////////////////
+app.get("/login",function(req,res){
+  res.render("login");
+});
 
+app.post("/login", passport.authenticate("local",{
+  successRedirect:"/blogs",
+  failureRedirect:"/login"
+}), function(req,res){
+});
 
+////////////////////////logout/////////////////////
 
+app.get("/logout",function(req,res){
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn(req,res,next){
+  if(req.isAuthenticated()){
+    return next();
+  }else{
+    res.redirect("/login");
+  }
+}
 
 
 
